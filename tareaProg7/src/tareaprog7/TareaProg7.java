e8f064c88a3996bbe4072cc08b2b592ae369bab3
Marcos Medina Castellano <Marcos Medina Castellano>
2016-04-24 00:39:19 +0100

    BlocNotas

---
+++ tareaProg7/src/tareaprog7/TareaProg7.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaprog7;
+
+/**
+ *
+ * @author Marcos
+ */
+public class TareaProg7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventanaPrincipal vPrincipal = new ventanaPrincipal();
+        vPrincipal.setLocationRelativeTo(null);
+        vPrincipal.pack();
+        vPrincipal.setTitle("Editor de texto Marcos");
+        vPrincipal.setVisible(true);
+    }
+    
+}
